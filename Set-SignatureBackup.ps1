83f74f27671eb5a5817866a00eb60c068439010d
MikeRoss <mike@aolccbc.com>
2022-12-08 10:27:09 -0800

    init

---
+++ Set-SignatureBackup.ps1
@@ -0,0 +1,22 @@
+function Test-ReparsePoint([string]$path) {
+    $file = Get-Item $path -Force -ea SilentlyContinue
+    return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+}
+
+$localsig = "$env:APPDATA\Microsoft\Signatures\"
+$ODSig = "$ENV:OneDriveCommercial\Signatures\"
+if((Test-ReparsePoint -path $localsig) -eq $true) {
+    Write-Host "Nothing To Do"
+} else {
+   if((Test-Path -Path $ODSIg) -eq $false) {
+       Write-Host need to make the folder
+       New-Item -ItemType Folder -Path $ODSig
+       }
+Get-ChildItem -Path $localsig | Copy-Item -Destination $ODSig -Container -Recurse -Force
+Rename-Item -Path $localsig -NewName "$localsig\Signatures.bak"
+$CommandList = "`"New-Item -ItemType SymbolicLink -Path '" + $localsig + "' -Target `'" + $ODSig + "`'"
+#$CommandList = Read-Host -Prompt "HI"
+Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $CommandList
+
+}
+
